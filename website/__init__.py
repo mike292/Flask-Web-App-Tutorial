0ec3adb87572462f1e306e54a0004564c5f36b42
mike292 <mfyorro@gmail.com>
2021-08-31 13:37:13 +0800

    From my pc

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'sdasdadas dsdasd'
+    # setup location of the database
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)  # initialize the database to this app
 
+    # import the variable views/auth(blueprint) from the module .views/.auth(file)
     from .views import views
     from .auth import auth
 
+    # Register the blueprint variable on the app
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
--- website/views.py
+++ website/views.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
 
+# Create the blueprint variable
 views = Blueprint('views', __name__)
 
-@views.route('/')
+
+@views.route('/')  # Create a route(URL) to the blueprint variable
+# Create a function that executes when the URL has been called
 def home():
     # return "<h1>Test</h1>"
-    return render_template("home.html")
+    return render_template("home.html")  # Returns a rendered html
