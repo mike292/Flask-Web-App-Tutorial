e08e4266c3936fa567c062a95092b2b1e83ab856
Michael <mfyorro@gmail.com>
2021-08-31 21:00:30 +0800

    Finnished

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from website import create_app
 
 app = create_app()
 
-#Run app only if this file main.py is executed not if imported to another
+# Run app only if this file main.py is executed not if imported to another
 if __name__ == '__main__':
-    app.run(debug=True) #To auto restart app when change in python code
-
+    app.run(debug=True)  # To auto restart app when change in python code
--- website/views.py
+++ website/views.py
@@ -1,15 +1,48 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
+from flask.json import jsonify
 from flask_login import login_required, current_user
 from sqlalchemy.sql.functions import user
+from . import db
+from .models import Note
+import json
 
 # Create the blueprint variable
 views = Blueprint('views', __name__)
 
 
-@views.route('/')  # Create a route(URL) to the blueprint variable
+# Create a route(URL) to the blueprint variable
+@views.route('/', methods=['GET', 'POST'])
 @login_required  # annot access this route if not login
 # Create a function that executes when the URL has been called
 def home():
     # return "<h1>Test</h1>"
+
+    if request.method == 'POST':
+        # data = request.form
+        # print(data)
+        note = request.form.get('note')
+        if len(note) < 1:
+            flash('The note is too short!', category='error')
+        else:
+            print(note, current_user.id)
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note has been added.', category='success')
+
     # Returns a rendered html
     return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+            flash('You have deleted a note!', category='success')
+
+    return jsonify({})
